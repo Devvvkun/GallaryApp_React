51001732d07f933fb7bb1348c437fb57ec31f30c
Devvvkun <himanshusinghsvmsln@gmail.com>
2026-09-17 00:11:36 +0530

    Working on the Gallery image view optimisation

--- src/GalleryApp.jsx
+++ src/GalleryApp.jsx
@@ -1,23 +1,14 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const GalleryApp = () => {
   const [userData, setUserData] = useState([]);
-
-  async function getData() {
-    const response = await axios.get(
-      "https://picsum.photos/v2/list?page=2&limit=15"
-    );
-
-    setUserData(response.data);
-  }
-
-  return (
-    <div>
-      <button onClick={getData}>Get Data</button>
-
-      <div className="w-full h-screen mx-4 flex gap-x-4 flex-wrap overflow-hidden">
-        {userData.map((elem, idx) => {
+  useEffect(function (){
+    getData();
+  },[])
+  const printUserData = "User Not Found"
+  if(userData.length>0){
+    printUserData = {userData.map((elem, idx) => {
           return (
             <div className="h-[25%] w-[17%] rounded-sm p-0 m-0" key={idx}>
               <a href={elem.url}>
@@ -30,6 +21,20 @@ const GalleryApp = () => {
             </div>
           );
         })}
+  }
+  async function getData() {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=15"
+    );
+    setUserData(response.data);
+  }
+    
+  return (
+    <div className="bg-black h-screen overflow-auto ">
+      
+
+      <div className=" flex gap-4 flex-wrap py-2">
+        {printUserData}
       </div>
     </div>
   );
